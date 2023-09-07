95e5baef9831ab30c359bc540abf638eac47d25c
strawbrryyjam <jhowells234@gmail.com>
2023-09-07 12:23:50 +1000

    player choice and computer choice
    Converted both player choice and computer choice to an integer for easy calculation.

--- code.js
+++ code.js
@@ -2,23 +2,42 @@ function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let computerChoice = Math.floor(Math.random() * choices.length);
 
-    console.log(choices[computerChoice]);
-}
+    let computer = (choices[computerChoice]);
+    let computer_second = computer.toLowerCase();
 
-function playRound(playerSelection) {
-    let playerChoice = playerSelection.toLowerCase();
+    if (computer_second == "rock") {
+        computer_second = 1;
+        return computer_second;
 
-    if (playerChoice == "rock") {
-        console.log("rock");
+    } else if (computer_second == "scissors") {
+        computer_second = 2;
+        return computer_second;
 
-    } else if (playerChoice == "scissors") {
-        console.log("scissors");
-
-    } else if (playerChoice == "paper") {
-        console.log("paper");
-    } else {
-        console.log("null");
+    } else if (computer_second == "paper") {
+        computer_second = 3;
+        return computer_second;
     }
 }
 
-playRound("paper");
+function playerChoice(playerSelection, getComputerChoice) {
+    let playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice == "rock") {
+        playerChoice = 1;
+
+    } else if (playerChoice == "scissors") {
+        playerChoice = 2;
+
+    } else if (playerChoice == "paper") {
+        playerChoice = 3;
+    }
+
+    console.log(playerChoice, getComputerChoice);
+
+    if (playerChoice == getComputerChoice) {
+        console.log("PLEASE");
+    }
+}
+
+playerChoice("rock", getComputerChoice())
+
