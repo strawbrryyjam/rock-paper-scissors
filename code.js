12e34bd711c4c3cd0a2c2c089e394dc86ed88657
strawbrryyjam <jhowells234@gmail.com>
2023-09-07 17:43:50 +1000

    Rounds function.
    Added rounds which the game finishes when the player or computer scores 5

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let computerChoice = Math.floor(Math.random() * choices.length);
@@ -11,30 +14,49 @@ function getComputerChoice() {
 function playerChoice(playerSelection, getComputerChoice) {
     let playerChoice = playerSelection.toLowerCase();
 
-    console.log(playerChoice, getComputerChoice)
-
     if (playerChoice == getComputerChoice) {
         console.log("it's a tie!")
+        playerScore += 1
+        computerScore += 1
+
     } else if (playerChoice == "rock" && getComputerChoice == "scissors") {
         console.log("rock beats scissors. YOU WIN!")
+        playerScore += 1
 
     } else if (playerChoice == "rock" && getComputerChoice == "paper") {
         console.log("paper beats rock. YOU LOSE")
+        computerScore += 1
 
     } else if (playerChoice == "scissors" && getComputerChoice == "rock") {
         console.log("rock beats scissors. YOU LOSE")
+        computerScore += 1
 
     } else if (playerChoice == "scissors" && getComputerChoice == "paper") {
         console.log("scissors beats paper. YOU WIN!")
+        playerScore += 1
 
     } else if (playerChoice == "paper" && getComputerChoice == "rock") {
         console.log("paper beats rock. YOU WIN!")
-        
+        playerScore += 1
+
     } else if (playerChoice == "paper" && getComputerChoice == "scissors") {
         console.log("scissors beats paper. YOU LOSE.")
+        computerScore += 1
+
     }
 
 }
 
-playerChoice("paper", getComputerChoice())
 
+function game() {
+    console.log("First to 5 wins..")
+    console.log("Round 1. FIGHT!");
+    
+    for (let rounds = 0; computerScore < 5 & playerScore < 5 ; rounds++) {
+        let round1 = prompt();
+        playerChoice(round1, getComputerChoice())
+        console.log("the score is " + playerScore + "-" + computerScore)
+    }
+}
+
+game()
