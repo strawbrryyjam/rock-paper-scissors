548d7640122ac053f3b044220f78728187ae2f9d
strawbrryyjam <jhowells234@gmail.com>
2023-09-07 11:32:55 +1000

    player selection function
    function so that user can select a choice from rock paper or scissors. A choice other than the previous returns "null".

--- code.js
+++ code.js
@@ -5,4 +5,20 @@ function getComputerChoice() {
     console.log(choices[computerChoice]);
 }
 
-getComputerChoice()
+function playRound(playerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice == "rock") {
+        console.log("rock");
+
+    } else if (playerChoice == "scissors") {
+        console.log("scissors");
+
+    } else if (playerChoice == "paper") {
+        console.log("paper");
+    } else {
+        console.log("null");
+    }
+}
+
+playRound("paper");
